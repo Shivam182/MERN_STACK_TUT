7f4ec5f0847c782acdb83d47580ed84d3b94cbd0
Shivam <shivam@gmail.com>
2022-01-08 18:24:02 +0530

    Adding database to ExpressJS

--- 3_expressJS&MongooseTutorial/express/4_routerApplication.js
+++ 3_expressJS&MongooseTutorial/express/4_routerApplication.js
@@ -6,4 +6,3 @@ const database = require('../mongoose/database');
 // For a complete section of ScienceBlogs , we handle its child routes like this . Instead of writing every route here 
 app.use('/ScienceBlogs',application)
 app.listen(5000);
-

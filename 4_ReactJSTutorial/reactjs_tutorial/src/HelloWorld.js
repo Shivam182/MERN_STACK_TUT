051c071a612ecc5fa89e82bfa88bb966944bd343
Shivam <shivam@gmail.com>
2022-01-10 12:01:26 +0530

    nyCMt

---
+++ 4_ReactJSTutorial/reactjs_tutorial/src/HelloWorld.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+class HelloWorld extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello World , I am Shivam !! </h1>
+            </div>
+        )
+    }
+}
+
+export default HelloWorld;
+
+/**
+ * Notes :-
+ * 
+ * 1. What is render function ?
+ * Ans : 
+ */
